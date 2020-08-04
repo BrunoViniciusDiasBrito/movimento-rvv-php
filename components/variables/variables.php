2079bdf3f3c5d31158f8f52df22042b5aec63dee
Bruno Brito <brunovinicius31121995@gmail.com>
2020-08-03 21:08:12 -0300

    videos updated variables updated

---
+++ components/variables/variables.php
@@ -0,0 +1,28 @@
+<?php 
+        $thumb = array(
+            array(
+                "title"=>"Basta Acreditar",
+                "link"=>"https://www.youtube.com/embed/F4c7cfLXH7I"
+            ),
+            array(
+                "title"=>"Outro Rumo",
+                "link"=>"https://www.youtube.com/embed/7pBdYOkT9DI"
+            ),
+            array(
+                "title"=>"Inteiramente Teu",
+                "link"=>"https://www.youtube.com/embed/mXJVGNaewuQ"
+            ),
+            array(
+                "title"=>"Deus é Top",
+                "link"=>"https://www.youtube.com/embed/rhVAX4Z75CQ"
+            ),
+            array(
+                "title"=>"Me Amas",
+                "link"=>"https://www.youtube.com/embed/ItZS9jdXpz4"
+            ),
+            array(
+                "title"=>"Eu quero ficar",
+                "link"=>"https://www.youtube.com/embed/k4-ePThZNmM"
+            )
+        )
+        ?>
--- index.php
+++ index.php
@@ -1,33 +1,6 @@
 <?php include './components/header.php' ?>
 
-        <?php 
-        $thumb = array(
-            array(
-                "title"=>"Basta Acreditar",
-                "link"=>"https://www.youtube.com/embed/F4c7cfLXH7I"
-            ),
-            array(
-                "title"=>"Outro Rumo",
-                "link"=>"https://www.youtube.com/embed/7pBdYOkT9DI"
-            ),
-            array(
-                "title"=>"Inteiramente Teu",
-                "link"=>"https://www.youtube.com/embed/mXJVGNaewuQ"
-            ),
-            array(
-                "title"=>"Deus é Top",
-                "link"=>"https://www.youtube.com/embed/rhVAX4Z75CQ"
-            ),
-            array(
-                "title"=>"Me Amas",
-                "link"=>"https://www.youtube.com/embed/ItZS9jdXpz4"
-            ),
-            array(
-                "title"=>"Eu quero ficar",
-                "link"=>"https://www.youtube.com/embed/k4-ePThZNmM"
-            )
-        )
-        ?>
+        <?php include './components/variables/variables.php' ?>
         
 
         <main class="main-wrap">   
