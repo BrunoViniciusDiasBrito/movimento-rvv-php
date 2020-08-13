fc42b01f714bb769e09488e362ecdc38a6bff8fb
Bruno Brito <brunovinicius31121995@gmail.com>
2020-08-13 13:29:15 -0300

    carousel v3

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
         
         <main class="main-wrap" id="main">   
 
-            <div class="pb-4 my-carousel">
+            <div class="pb-1 my-carousel">
                 
                 <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade" data-ride="carousel">
                     <ol class="carousel-indicators">
@@ -26,6 +26,21 @@
                 
             </div>           
                         
+            <div class="container py-5">
+                <div class="row">
+                    <div class="col-12">
+                        <h1>Um pouco sobre nós</h1>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum non diam in arcu feugiat euismod a eget est. Quisque aliquet, nisl id sollicitudin porta, libero arcu suscipit dui, sit amet vestibulum dolor turpis quis odio. Sed tristique vel sapien vitae malesuada. Morbi eu dui magna. Proin sit amet ligula a ante eleifend faucibus quis et velit. In cursus sodales justo at efficitur. Mauris maximus posuere nulla, in mollis nibh imperdiet et. Fusce a ex purus.
+
+                        Proin vulputate id sapien at posuere. Duis posuere lorem ac risus ultricies efficitur. Sed laoreet purus vel lacinia finibus. Nullam in lobortis ipsum, a aliquet dolor. Pellentesque quis ligula in risus cursus vulputate. Curabitur nec odio suscipit, efficitur nisl in, interdum velit. Mauris venenatis nisl id molestie vulputate. Curabitur et semper mi. Praesent suscipit ipsum sed erat iaculis, in venenatis elit viverra. Fusce in pretium lacus, placerat fringilla risus. Ut venenatis massa eu ante elementum, sed bibendum mi aliquet. Mauris egestas dolor dui, in luctus odio venenatis in. Pellentesque ultrices, elit eget congue accumsan, ipsum dolor blandit justo, ut viverra augue arcu id felis.
+
+                        Ut accumsan vehicula ornare. Aliquam finibus non odio vel placerat. Proin at aliquam ante. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse fringilla eros diam, sed consectetur sem tincidunt vel. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed sed leo ac metus ullamcorper vehicula at nec risus. Quisque eget ullamcorper ipsum. Nam id mi libero. Mauris cursus dictum erat, eget posuere leo luctus sit amet.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
             <article class="optin">
                     <div class="adjust">
                         <h2>Receba notificações das nossas atividades :)</h2>
@@ -37,8 +52,6 @@
                         </form>
                     </div>
             </article>
-
-           
 
             <article class="video_section py-5">
                 <div class="adjust_videos">
