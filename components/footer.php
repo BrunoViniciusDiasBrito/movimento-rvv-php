c5df8c900840d1aa45f500b4aead225c864f6844
Bruno Brito <brunovinicius31121995@gmail.com>
2020-08-14 19:14:56 -0300

    footer v2

--- components/footer.php
+++ components/footer.php
@@ -1,21 +1,21 @@
 
-        <footer class="footer py-2">
+        <footer class="footer pt-5 pb-3">
             <div class="container">
                 <div class="row d-flex justify-content-center">
                     <div class="col-10 col-md col-lg-4 contact">
-                        <h1 class="mb-1">O que fazemos?</h1>
+                        <h1 class="mb-3">O que fazemos?</h1>
                         <p class="mb-0">Projetos socias</p>
                         <p class="mb-0">Retiros Espirituais</p>
                         <p>Música</p>
                     </div>
                     <div class="col-10 col-md col-lg-4 contact midias">
-                        <h1>Nossas Redes Sociais</h1>
-                        <span><i class="fab fa-instagram insta mt-1"></i> <a href="#">@movimentorevivia</a></span>
-                        <span><i class="fab fa-facebook face"></i> <a href="#">/movimentorevivia</a></span>
-                        <span><i class="fab fa-youtube yt"></i> <a href="#">movimentorevivia</a></span>
+                        <h1 class="mb-3">Nossas Redes Sociais</h1>
+                        <span><i class="fab fa-instagram insta mt-1"></i> <a href="https://www.instagram.com/movimentoreviva/">@movimentorevivia</a></span>
+                        <span><i class="fab fa-facebook face"></i> <a href="https://www.facebook.com/movimentoreviva/?epa=SEARCH_BOX">/movimentorevivia</a></span>
+                        <span><i class="fab fa-youtube yt"></i> <a href="https://www.youtube.com/channel/UCBx_Pl-SimtI6KNdgo481HQ">movimentorevivia</a></span>
                     </div>
                     <div class="col-10 col-md col-lg-4 contact mensagem">
-                        <h1>Sal da terra</h1>
+                        <h1 class="mb-3">Sal da terra</h1>
                         <p>"Vocês são o sal da terra. Mas, se o sal perder o seu sabor, como restaurá-lo? Não servirá para nada, exceto para ser jogado fora e pisado pelos homens.</p>
                     </div>
                 </div>
